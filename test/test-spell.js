8acc66137878f1d844aaadb797687be342036e1d
KhaledSami <k.sami.mohammed@gmail.com>
2015-05-16 16:12:52 +0200

    fix the test for loadDict function

--- test/test-spell.js
+++ test/test-spell.js
@@ -13,7 +13,7 @@ suite("Spell Corrector", function() {
         assert.equal(spellCorrector.nWords.count(), 0);
     });
 
-    test("should construct a hashmap from the passed dictionary", function() {
+    test("loadDict function should construct a hashmap from the passed dictionary", function() {
         spellCorrector.loadDict('./dictionary/small.txt');
         var nWords = spellCorrector.nWords;
         assert.equal(nWords.get("the"), 2);
