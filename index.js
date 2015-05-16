5b067424664a0c0abccca0a137a88871cab22c7a
KhaledSami <k.sami.mohammed@gmail.com>
2015-05-17 00:27:50 +0200

    wrapping the application in spellCorrector.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var SpellCorrector = require('./src/spell.js').spellCorrector;
-
-var spellCorrector = new SpellCorrector();
-
-spellCorrector.loadDict();
-
-console.log(spellCorrector.correct("spel"));
-
---
+++ spellCorrector.js
@@ -0,0 +1,1 @@
+module.exports = require('./src/spell.js').spellCorrector;
