08498aebebd938ab48a7617c5968be91b889bfcc
KhaledSami <k.sami.mohammed@gmail.com>
2015-05-16 16:10:09 +0200

    generate all possible corrections from a word

--- src/spell.js
+++ src/spell.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 
 var spellCorrector = function() {
     this.nWords = new HashMap();
+    this.alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 }
 
 spellCorrector.prototype.loadDict = function(dictPath) {
@@ -18,6 +19,17 @@ spellCorrector.prototype.loadDict = function(dictPath) {
         }
         this.nWords.set(word, newCount);
     }
+}
+
+spellCorrector.prototype.getEdits = function(word) {
+    var result = [];
+    for (var i = 0; i < word.length; i++) result.push(word.slice(0, i) + word.slice(i + 1));
+    for (var i = 0; i < word.length - 1; i++) result.push(word.slice(0, i) + word.slice(i + 1, i + 2) + word.slice(i, i + 1) + word.slice(i + 2));
+    for (var i = 0; i < word.length; i++)
+        for (var j = 0; j < this.alphabet.length; j++) result.push(word.slice(0, i) + this.alphabet[j] + word.slice(i + 1));
+    for (var i = 0; i < word.length; i++)
+        for (var j = 0; j < this.alphabet.length; j++) result.push(word.slice(0, i) + this.alphabet[j] + word.slice(i));
+    return result;
 }
 
 module.exports.spellCorrector = spellCorrector;
