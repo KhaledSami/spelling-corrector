d05e9de1db6b431986f03a6a3a68dd7fb5a6c728
KhaledSami <k.sami.mohammed@gmail.com>
2015-05-16 17:04:26 +0200

    adding the correction function

--- src/spell.js
+++ src/spell.js
@@ -7,7 +7,7 @@ var spellCorrector = function() {
 }
 
 spellCorrector.prototype.loadDict = function(dictPath) {
-    dictPath = dictPath || '../dictonary/big.txt';
+    dictPath = dictPath || './dictionary/big.txt';
     var file = fs.readFileSync(dictPath).toString().toLowerCase();
     var regex = /[a-z]+/g;
     var match, word;
@@ -31,5 +31,45 @@ spellCorrector.prototype.getEdits = function(word) {
         for (var j = 0; j < this.alphabet.length; j++) result.push(word.slice(0, i) + this.alphabet[j] + word.slice(i));
     return result;
 }
+
+spellCorrector.prototype.correct = function(word) {
+    if (this.nWords.has(word)) {
+        return word;
+    }
+    var suggestions = this.getEdits(word);
+    var candidates = new HashMap();
+    suggestions.forEach(function(curWord) {
+        if (this.nWords.has(curWord)) {
+            candidates.set(this.nWords.get(curWord), curWord);
+        }
+    }.bind(this));
+
+    if (candidates.count() > 0) {
+        return bestCandidate(candidates);
+    }
+
+    suggestions.forEach(function(curWord) {
+        var newSuggestions = this.getEdits(curWord);
+        newSuggestions.forEach(function(newWord) {
+            if (this.nWords.has(newWord)) {
+                candidates.set(this.nWords.get(newWord), newWord);
+            }
+        }.bind(this));
+    }.bind(this));
+    return (candidates.count() > 0) ? bestCandidate(candidates) : word;
+}
+
+function bestCandidate(candidates) {
+    var maxCount = 0;
+    var word;
+    candidates.forEach(function(value, key) {
+        if (key > maxCount) {
+            maxCount = key;
+            word = value;
+        }
+    });
+    return word;
+}
+
 
 module.exports.spellCorrector = spellCorrector;
